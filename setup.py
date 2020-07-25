597eb4bbe719fdd14decbdbaedba907b93ca5fcd
Paul Glass <pnglass@gmail.com>
2020-07-25 00:15:24 -0500

    Update package metadata + license

--- setup.py
+++ setup.py
@@ -5,22 +5,24 @@ long_description = open('README.md').read()
 setup_params = dict(
     name='py-openapi-schema-to-json-schema',
     version='0.0.2',
+    license='MIT',
     author='Paul Glass',
     author_email='pnglass@gmail.com',
-    url='https://github.com/pglass/py-openapi-jsonschema-converter',
+    url='https://github.com/pglass/py-openapi-schema-to-json-schema',
     keywords='openapi json schema convert translate',
     packages=['openapi_schema_to_json_schema'],
-    description='translate openapi schemas to json schemas',
+    description='Convert OpenAPI Schemas to JSON Schemas',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
 
